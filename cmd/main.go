51917b308fb4feb11b357abc1f9e1ae75a221ef1
BohuTANG <overred.shuttler@gmail.com>
2021-05-20 11:49:05 +0800

    [cmd] add labeler

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"os"
+	"strings"
 
 	"net/http"
 
 	"github.com/go-playground/webhooks/v6/github"
+	"github.com/jimschubert/labeler"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -13,12 +16,25 @@ const (
 	path = "/webhooks"
 )
 
+/*
+* export GITHUB_SECRET='don't tell'
+* export GITHUB_TOKEN='don't tell'
+* export GITHUB_REPO='datafuselabs/datafuse'
+*/
 func main() {
-	token, found := os.LookupEnv("GITHUB_TOKEN")
+	// Check GITHUB_SECRET.
+	secret, found := os.LookupEnv("GITHUB_SECRET")
+	if !found {
+		log.Fatal("Missing input 'GITHUB_SECRET' env.")
+	}
+
+	// Check GITHUB_TOKEN.
+	_, found = os.LookupEnv("GITHUB_TOKEN")
 	if !found {
 		log.Fatal("Missing input 'GITHUB_TOKEN' env.")
 	}
-	hook, _ := github.New(github.Options.Secret(token))
+
+	hook, _ := github.New(github.Options.Secret(secret))
 
 	http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
 		payload, err := hook.Parse(r, github.ReleaseEvent, github.PullRequestEvent)
@@ -34,8 +50,23 @@ func main() {
 			log.Infof("%+v", release)
 
 		case github.PullRequestPayload:
-			pullRequest := payload.(github.PullRequestPayload)
-			log.Infof("%+v", pullRequest)
+			pr := payload.(github.PullRequestPayload)
+			log.Infof("Pull reqeust id: %+v", pr.Number)
+			repo := os.Getenv("GITHUB_REPO")
+			repoParts := strings.Split(repo, "/")
+			owner := repoParts[0]
+			repoName := repoParts[1]
+			body, _:= json.Marshal(pr)
+			data := string(body)
+			l, err := labeler.New(owner, repoName, "pull_request", int(pr.Number), &data)
+			if err != nil {
+				log.Errorf("Could not construct a labeling: %v", err)
+			}
+			if l.Execute()!= nil {
+				log.Errorf("Failed to execute labeling: %v", err)
+			}
+
+			log.Info("Done labeling.")
 		}
 	})
 	http.ListenAndServe(":3000", nil)
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/go-playground/webhooks/v6 v6.0.0-beta.3
+	github.com/jimschubert/labeler v0.0.3
 	github.com/sirupsen/logrus v1.8.1
 )
